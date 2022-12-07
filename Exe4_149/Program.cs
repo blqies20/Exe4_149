d175b601542950c1c68108bb5905c14eb2aceef9
ACER <ACER@DESKTOP-QH5VR0V>
2022-12-07 15:49:05 +0700

    add public void push

--- Exe4_149/Program.cs
+++ Exe4_149/Program.cs
@@ -26,5 +26,14 @@ namespace Exe4_149
             else
                 return (false);
         }
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + "pushed.");
+        }
     }
 }
