9f10eaf3519c60497ec7c655fe58152e5930a253
ACER <ACER@DESKTOP-QH5VR0V>
2022-12-07 15:53:47 +0700

    add ublic void display

--- Exe4_149/Program.cs
+++ Exe4_149/Program.cs
@@ -40,5 +40,19 @@ namespace Exe4_149
             top = fresh;
             Console.WriteLine("\n" + element + "pushed.");
         }
+        public void display()
+        {
+            Node tmp;
+            if (empty())
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
